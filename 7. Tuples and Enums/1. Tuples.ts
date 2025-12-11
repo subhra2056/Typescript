aecaaf48e4d39fe30444882f3e061775bc035d56
subhra2056 <encrypted22.11@gmail.com>
2025-12-11 23:43:05 +0530

    Tuples and Enums

---
+++ 7. Tuples and Enums/1. Tuples.ts
@@ -0,0 +1,10 @@
+// let myTuple: [number, string, string] = [1, "apple", "ball"];
+// // let myTuple: [number, string, string] = [1, "apple", "ball", 1]; // This will throw error
+
+// console.log(myTuple[0]);
+
+const products: (number | string)[] = ['item1', 12];
+console.log(products);
+
+const games: [string, number, boolean] = ['game1', 3, true];
+console.log(games);
---
+++ 7. Tuples and Enums/2. Enums.ts
@@ -0,0 +1,9 @@
+enum WeekDays {
+  Monday = "Monday",
+  Tuesday = "Tuesday",
+  Wednesday = "Wednesday",
+  Thursday = "Thursday"
+}
+
+const currentDate = WeekDays.Monday;
+console.log(`Today is ${currentDate}`);
