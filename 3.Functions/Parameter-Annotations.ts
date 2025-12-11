334d2f37872d20b08d3921dcdbc995449c478197
subhra2056 <encrypted22.11@gmail.com>
2025-12-11 07:01:07 +0530

    functions

---
+++ 3.Functions/Parameter-Annotations.ts
@@ -0,0 +1,14 @@
+//Regular func
+
+// function addOne(num: number){
+//     return num + 1;
+// }
+
+// const res = addOne(5);
+// console.log(res);
+
+//Arrow Func
+
+const double = (x: number, y: number) => x * y;
+const res = double(3, 4);
+console.log(res);
