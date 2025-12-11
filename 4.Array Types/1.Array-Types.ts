fc4d09ce4ed65ac3953fc985d5b6834bf016f4e7
subhra2056 <encrypted22.11@gmail.com>
2025-12-11 07:28:00 +0530

    Arrays

---
+++ 4.Array Types/1.Array-Types.ts
@@ -0,0 +1,9 @@
+const numbers: number[] = [1, 2, 3, 4];
+console.log(numbers);
+
+const numbersOne: any[] = [1, 2, [1, 2], 'String'];
+console.log(numbersOne);
+
+const numbersTwo: string[] = ['one', 'two', 'three'];
+numbersTwo.push('four');
+console.log(numbersTwo);
---
+++ 4.Array Types/2.Multidimensional-Arrays.ts
@@ -0,0 +1,7 @@
+const oneDi: number[] = [1, 2, 3];
+const twoDi: number[][] = [[1, 2], [3, 4]];
+const threeDi: number[][][] = [[[1, 2],[1, 2]],[[3, 4],[3, 4]]];
+
+console.log(oneDi);
+console.log(twoDi);
+console.log(threeDi);
